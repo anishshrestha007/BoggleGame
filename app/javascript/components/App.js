b621306863b95d31ffb645c2216fbf1bd5df46f2
anish.shrestha <anishronnie@gmail.com>
2020-03-16 17:58:18 +0545

    Added DashBoard changes.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -5,6 +5,9 @@ import BoogleGame from "./game/BoggleGame";
 import { Provider } from "react-redux";
 import configureStore from "../store/configureStore";
 import DashBoard from "./game/DashBoard";
+import GameLogo from "../../assets/images/GameLogo.png";
+import { Image } from "semantic-ui-react";
+
 const store = configureStore();
 class App extends React.Component {
   render() {
@@ -12,11 +15,14 @@ class App extends React.Component {
       <Provider store={store}>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" render={() => <DashBoard />} />
-            <Route
-              path="/game"
-              render={() => <BoogleGame boggleParam="Hello from game!" />}
-            />
+            <React.Fragment>
+              <Image src={GameLogo} wrapped ui={false} />
+              <Route exact path="/" render={() => <DashBoard />} />
+              <Route
+                path="/game"
+                render={() => <BoogleGame boggleParam="Hello from game!" />}
+              />
+            </React.Fragment>
           </Switch>
         </BrowserRouter>
       </Provider>
--- app/javascript/components/game/DashBoard.js
+++ app/javascript/components/game/DashBoard.js
@@ -1,11 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Input } from "semantic-ui-react";
+import { Input, Header, Card, Icon } from "semantic-ui-react";
 class DashBoard extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Input label="http://" placeholder="mysite.com" />
+        <Card centered color="orange" raised={true}>
+          <Card.Content>
+            <Card.Header color="orange"> Boggle Game </Card.Header>
+            <Card.Description>
+              Boggle is a word game invented by Allan Turoff and originally
+              distributed by Parker Brothers.The game is played using a plastic
+              grid of lettered dice, in which players attempt to find words in
+              sequences of adjacent letters.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Input
+              icon="user"
+              iconPosition="left"
+              label="UserName"
+              placeholder="Enter you name here"
+            />
+          </Card.Content>
+        </Card>
       </React.Fragment>
     );
   }
