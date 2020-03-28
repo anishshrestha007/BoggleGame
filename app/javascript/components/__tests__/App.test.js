c6dd22f3ed73a5ee337de5a82093ae35de64eafa
Anish Shrestha <anishronnie@gmail.com>
2020-03-28 19:33:52 +0545

    Added test for aoo js

---
+++ app/javascript/components/__tests__/App.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import App from "../App";
+
+import { mount, shallow } from "enzyme";
+import GameLogo from "../../../assets/images/GameLogo.png";
+describe("Main App Testing", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+    console.log(wrapper.debug());
+  });
+
+  test("render the app component", () => {
+    expect(wrapper).toMatchSnapshot;
+  });
+  test("render image component", () => {
+    expect(wrapper).toBeTruthy();
+    expect(wrapper.find("Image").length).toEqual(1);
+  });
+  test("render SemanticToastContainer component", () => {
+    expect(wrapper).toBeTruthy();
+    expect(wrapper.find("SemanticToastContainer").length).toEqual(1);
+  });
+  test("render image component src", () => {
+    expect(wrapper.find("Image").prop("src")).toEqual(GameLogo);
+  });
+});
