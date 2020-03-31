04bd0e4150ee3ac6bf230aaf51ce5c646c8db0dd
Anish Shrestha <anishronnie@gmail.com>
2020-03-31 07:24:17 +0545

    Changed boggle game time to 3 minutes

--- config/constant/gameData.rb
+++ config/constant/gameData.rb
@@ -1,5 +1,5 @@
 MIN_WORD_LENGTH = 3
 MAX_WORD_LENGTH = 10
 BOGGLE_VERSION = "v1"
-GAME_TIME = 1
+GAME_TIME = 3
 BOGGLE_STRING = "qwertyuiopasdfghjklzxcvbnmmnbvctaasdffghjklpoiuitreqqwertouioplkjhgfdabncvbnmmnbvcbdasdfghjklpoiultreqasdfghjklmnbvcge"
